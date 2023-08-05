98151557b25edb877efe573382558b98c2babb0d
Jakub Iliński <kubailinski2@gmail.com>
2023-08-05 11:45:52 +0200

    1x800, 2x1000, 1x1200, 1x1300 including my 500th solved problem 🥳🤩🎂🎉

---
+++ codeforces/problems/IQ test/cpp497.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int n;
+vector<int> v;
+void get_input(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n;
+
+    v.resize(n + 1);
+    for(int i = 1; i <= n; i++)
+        cin >> v[i];
+}
+int main(){
+    get_input();
+
+    int even = 0, odd = 0, find;
+    for(int i = 1; i <= 3; i++){
+        if(v[i] & 1) odd++;
+        else even++;
+    }
+
+    if(even > odd) find = 1;
+    else find = 0;
+
+    for(int i = 1; i <= n; i++)
+        if(v[i] % 2 == find) cout << i;
+    cout << endl; 
+}
