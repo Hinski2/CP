dd5cb6b755bf27895ac9204ad946c66259624780
Jakub Iliński <kubailinski2@gmail.com>
2023-08-31 15:53:27 +0200

    triple a

---
+++ codeforces/problems/Puzzles/cpp579.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int n, m;
+vector<int> v;
+void get_input(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n >> m;
+    for(int i = 0, x; i < m; i++){
+        cin >> x;
+        v.pb(x);
+    }
+}
+int main(){
+    get_input();
+    sort(all(v));
+
+    int ans = inf;
+    for(int i = 0; i <= m - n; i++)
+        ans = min(ans, v[i + n - 1] - v[i]);
+
+    cout << ans << endl;
+
+}
