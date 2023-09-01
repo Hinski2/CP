8c70678ee760ddb104aaeb2a2a928ef90214409c
Jakub Iliński <kubailinski2@gmail.com>
2023-09-01 17:36:53 +0200

    triple a

---
+++ codeforces/problems/The New Year: Meeting Friends/cpp583.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 105;
+
+vector<int> v;
+void get_input(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    for(int i = 0, x; i < 3; i++){
+        cin >> x;
+        v.pb(x);
+    }
+}
+int main(){
+    get_input();
+
+    int ans = inf, pos;
+    for(int i = 1; i < mak; i++){
+        int odl = 0;
+        for(auto u: v)
+            odl += abs(u - i);
+        ans = min(ans, odl);
+    }
+
+    cout << ans << endl;
+
+}
