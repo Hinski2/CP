dbd6a01854618d7e867b14edf6bb9d4164b4334f
Jakub Iliński <kubailinski2@gmail.com>
2023-09-06 16:01:39 +0200

    adding 600th solution for a problem and 3 others

---
+++ codeforces/problems/Sum/cpp598.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int t;
+void get_input(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> t;
+}
+
+bool solve(){
+    vector<int> v(3);
+    for(int i = 0; i < 3; i++)
+        cin >> v[i];
+    sort(all(v));
+
+    return v[0] + v[1] == v[2];
+}
+int main(){
+    get_input();
+    while(t--)
+        cout << (solve() ? "yes" : "no") << endl;
+}
