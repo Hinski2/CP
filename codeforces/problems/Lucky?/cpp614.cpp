142b099293df27ba4aaba371c69d2aa0dbcd6bb8
Jakub Iliński <kubailinski2@gmail.com>
2023-09-11 06:18:33 +0200

    triple a

---
+++ codeforces/problems/Lucky?/cpp614.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int t;
+void get_input(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> t;
+}
+
+bool solve(int x){
+    int s1 = 0, s2 = 0;
+    for(int i = 0; i < 6; i++){
+        if(i < 3) s1 += x % 10;
+        else s2 += x % 10;
+        x /= 10;
+    }
+
+    return s1 == s2;
+}
+int main(){
+    get_input();
+
+    int x;
+    while(t-- && cin >> x)
+        cout << (solve(x) ? "YES" : "NO") << endl;
+}
