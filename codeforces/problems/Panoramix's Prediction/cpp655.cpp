c001b26b0874183f7284915a9b8f2ccb5921629b
Jakub Iliński <kubailinski2@gmail.com>
2023-09-24 11:56:00 +0200

    triple a

---
+++ codeforces/problems/Panoramix's Prediction/cpp655.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 100;
+
+int n, m;
+vector<int> prime;
+
+void get_input(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+
+void make_primes(){
+    vector<bool> num(mak, 1);
+    for(int i = 2; i < mak; i++)
+        if(num[i]){
+            prime.pb(i);
+            for(int j = i * i; j < mak; j += i)
+                num[j] = false;
+        }
+}
+
+int main(){
+    get_input();
+    make_primes();
+
+    cin >> n >> m;
+    vector<int>::iterator it = upper_bound(prime.begin(), prime.end(), n);
+    cout << (m == *it ? "YES" : "NO") << endl;
+}
