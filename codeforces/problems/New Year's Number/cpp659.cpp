7f05033d24f638220d10c4ef3e17332ea599a95c
Jakub Iliński <kubailinski2@gmail.com>
2023-09-25 20:33:11 +0200

    icpc

---
+++ codeforces/problems/New Year's Number/cpp659.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int t;
+void get_input(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> t;
+}
+
+bool solve(int x){
+    int no1 = x / 2020, no2 = 0;
+    while(no1 >= 0){
+        if(no1 * 2020 + no2 * 2021 == x) return true;
+        no1--, no2++;
+    }
+
+    return false;
+}
+
+int main(){
+    get_input();
+    int x;
+    while(t-- && cin >> x)
+        cout << (solve(x) ? "YES" : "NO") << endl;
+}
