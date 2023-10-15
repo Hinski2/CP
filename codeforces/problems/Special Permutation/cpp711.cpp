fa831941605b5240266e7e7b7187e4c0927637fb
Jakub Iliński <kubailinski2@gmail.com>
2023-10-15 18:40:23 +0200

    triple a

---
+++ codeforces/problems/Special Permutation/cpp711.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int t;
+void get_input(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> t;
+}
+
+void solve(int x){
+    for(int i = x; i > 0; i--){
+        if(x & 1 && i == x / 2 + 1) cout << 1 << ' ';
+        else if(x & 1 && i == 1) cout << x / 2 + 1<< ' ';
+        else cout << i << ' ';
+    }
+    cout << endl;
+} 
+int main(){
+    get_input();
+    int x;
+    while(t-- && cin >> x)
+        solve(x);
+}
