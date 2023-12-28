16d0c2710993028c3781ffe045e79df326faf0f9
Jakub Iliński <kubailinski2@gmail.com>
2023-12-28 21:04:42 +0100

    triple a

---
+++ codeforces/problems/Reconnaissance 2/cpp912.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int main(){
+    io; int n; cin >> n;
+    vector<int> v(2 * n);
+    for(int i = 0; i < n; i++)
+        cin >> v[i];
+    for(int i = n; i < 2 * n; i++)
+        v[i] = v[i - n];
+    
+    int ans = inf, pos;
+    for(int i = 1; i < 2 * n; i++)
+        if(ans > abs(v[i] - v[i - 1])){
+            ans = abs(v[i] - v[i - 1]);
+            pos = i;
+        }
+
+    cout << (pos + n) % n + 1<< ' ' << (pos - 1 + n) % n + 1<< endl;
+
+}
