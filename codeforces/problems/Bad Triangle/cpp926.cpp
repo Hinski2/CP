bbe252379dd30f3bab3d22dc915f59abfc62a245
Jakub Iliński <kubailinski2@gmail.com>
2024-01-02 14:16:37 +0100

    triple a

---
+++ codeforces/problems/Bad Triangle/cpp926.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+void solve(){
+    int n; cin >> n;
+    vector<ll> v(n + 1);
+    for(int i = 1; i <= n; i++)
+        cin >> v[i];
+
+    ll suma = v[1] + v[2];
+    for(int i = 3; i <= n; i++){
+        if(suma <= v[i]){
+            cout << 1 << ' ' << 2 << ' ' << i << endl;
+            return;
+        }
+    }
+
+    cout << "-1" << endl;
+}
+
+int main(){
+    io; int t; cin >> t;
+    while(t--)
+        solve();
+}
---
+++ codeforces/problems/Three Piles of Candies/cpp925.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+ll solve(){
+    vector<ll> v(3); cin >> v;
+    sort(all(v));
+    ll a = 0, b = 0;
+    a += v[0];
+    b += v[1];
+
+    ll delta = b - a;
+    a += delta;
+    v[2] -= delta;
+    //mają tyle samo
+
+    a += v[2] / 2;
+    b += v[2] / 2;
+
+    return a;
+}
+
+int main(){
+    io; int t; cin >> t;
+    while(t--)
+        cout << solve() << endl;
+}
