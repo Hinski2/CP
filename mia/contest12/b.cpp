3f80d93f0dd693e24b05055c550073b5303e842b
Jakub Iliński <kubailinski2@gmail.com>
2024-01-09 16:09:00 +0100

    1x800, 1x1100, 1x2000

---
+++ mia/contest12/b.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int solve(){
+    int n; cin >> n;
+    vector<int> v(n); cin >> v;
+    if(n == 1) return 1;
+    int win = 0, samo1 = 1;
+
+    for(auto u: v) if(u != 1) samo1 = 0;
+    if(samo1 && (n & 1)) return 1;
+    else if(samo1) return 0; 
+
+    for(int i = n - 1; i >= 0; i--){
+        if(v[i] != 1) win = 1;
+        else win ^= 1;
+    }
+
+    return win;
+}
+
+int main(){
+    io; int t; cin >> t;
+    while(t--)
+        cout << (solve() ? "First" : "Second") << endl;
+}
