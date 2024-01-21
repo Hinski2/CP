06abace661777b12e5f460b4572f0dc041f10c25
Jakub Iliński <kubailinski2@gmail.com>
2024-01-21 15:06:11 +0100

    2x1100, 1x1400

---
+++ codeforces/problems/Parallelepiped/cpp976.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int main(){
+    io; int A, B, C; cin >> A >> B >> C;
+
+    for(int a = 1; a <= A; a++){
+        for(int b = 1; b <= B; b++){
+            if(a * b != A) continue;
+            if(C % a || B % b) continue;
+            if(B / b != C / a) continue;
+
+            int c = B / b;
+            cout << 4 * a + 4 * b + 4 * c << endl;
+            return 0;
+        }
+    }
+}
