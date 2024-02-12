328f707d13df9bf622f7c0ba8ecab37faaba039f
Jakub Iliński <kubailinski2@gmail.com>
2024-02-12 19:25:11 +0100

    triple a

---
+++ codeforces/problems/Cormen — The Best Friend Of a Man/cpp1040.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int main(){
+    io; int n, k; cin >> n >> k;
+    vector<int> v(n + 2);
+    for(int i = 1; i <= n; i++)
+        cin >> v[i];
+    v[0] = v[n + 1] = k;
+
+    int sum = k, ans = 0;
+    for(int i = 1; i <= n; i++){
+        sum += v[i];
+        if(i != 1) sum -= v[i - 2];
+
+        int add = max(0, k - sum);
+        sum += add;
+        v[i] += add;
+        ans += add;
+    }
+
+    cout << ans << endl;
+    for(int i = 1; i <= n; i++)
+        cout << v[i] << ' ';
+    cout << endl;
+}
---
+++ codeforces/problems/Phoenix and Puzzle/cpp1041.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+bool s1(int x){
+    if(x % 2) return 0;
+    x /= 2;
+    int s = sqrt(x);
+    return s * s == x;
+}
+
+bool s2(int x){
+    if(x % 4) return 0;
+    x /= 4;
+    int s = sqrt(x);
+    return s * s == x;
+}
+
+int main(){
+    io; int t; cin >> t;
+    while(t--){
+        int x; cin >> x;
+        cout << (s1(x) || s2(x) ? "YES\n" : "NO\n");
+    }
+}
