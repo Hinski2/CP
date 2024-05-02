4d9442af3904b710256b8fea6c2471e16ac71ebb
Hinski2 <kubailinski2@gmail.com>
2024-05-02 17:47:59 +0200

    1x1400

---
+++ codeforces/problems/Ball in Berland/cpp1268.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+ll solve(){
+    ll ans = 0, a, b, k; cin >> a >> b >> k;
+    vector<ll> va(a), vb(b);
+    vector<pii> edge(k);
+
+    for(auto &u: edge)
+        cin >> u.fi;
+    for(auto &u: edge)
+        cin >> u.se;
+
+    for(auto &u: edge){
+        va[--u.fi]++;
+        vb[--u.se]++;
+    }
+
+    for(auto &[x, y]: edge)
+        ans += k - va[x] - vb[y] + 1;
+    return ans / 2;
+}
+
+int main(){
+    io; int t; cin >> t;
+    while(t--)
+        cout << solve() << endl;
+}
