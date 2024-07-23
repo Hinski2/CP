e934ec83add4745f6a524ecf0d464a1e7e1a7542
Hinski2 <kubailinski2@gmail.com>
2024-07-23 17:40:04 +0200

    1x1400

---
+++ codeforces/problems/News Distribution/cpp1367.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator+(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first + p2.first, p1.second + p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator-(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first - p2.first, p1.second - p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator*(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first * p2.first, p1.second * p2.second);
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) {
+    os << p.first << ' ' << p.second;
+    return os;
+}
+template <typename T1, typename T2>
+istream& operator>>(istream& is, pair<T1, T2>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int n, m;
+vector<int> p;
+vector<int> sajz;
+
+int find(int x){
+    return (x == p[x] ? x : find(p[x]));
+}
+
+void onion(int a, int b){
+    a = find(a), b = find(b);
+    if(a == b) return;
+    if(sajz[a] < sajz[b]) swap(a, b);
+    sajz[a] += sajz[b];
+    p[b] = a;
+}
+
+int main(){
+    io; cin >> n >> m;
+    sajz.resize(n + 1, 1);
+    p.resize(n + 1); for(int i = 1; i <= n; i++) p[i] = i;
+
+    for(int i = 0; i < m; i++){
+        int x; cin >> x;
+        int last = -1;
+        for(int j = 0, y; j < x; j++){
+            cin >> y;
+            if(last != -1)
+                onion(y, last);
+            last = y;
+        }
+    }
+
+    for(int i = 1; i <= n; i++)
+        cout << sajz[find(i)] << ' ';
+    cout << endl;
+}
