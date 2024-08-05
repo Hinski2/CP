1919fec1d2e8d3a437c0b06e726430f011110d16
Hinski2 <kubailinski2@gmail.com>
2024-08-05 11:06:30 +0200

    2x800, 1x1200

---
+++ codeforces/problems/Stable Groups/cpp1385.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator+(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first + p2.first, p1.second + p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator-(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first - p2.first, p1.second - p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator*(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first * p2.first, p1.second * p2.second);
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) {
+    os << p.first << ' ' << p.second;
+    return os;
+}
+template <typename T1, typename T2>
+istream& operator>>(istream& is, pair<T1, T2>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+#define int ll
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int n, k, x;
+vector<int> v;
+signed main(){
+    io; cin >> n >> k >> x;
+    v.resize(n); cin >> v;
+    sort(all(v));
+
+    vector<int> diff;
+    for(int i = 0; i < n - 1; i++)
+        diff.pb(v[i + 1] - v[i]);
+    
+    sort(all(diff));
+    int cnt = 1;
+    for(auto u: diff)
+        if(u > x && k){
+            int need = (u + x - 1) / x - 1;
+            if(need <= k) k-= need;
+            else k = 0, cnt++;
+        }
+        else if(u > x) cnt++;
+    
+    cout << cnt << endl;
+}
