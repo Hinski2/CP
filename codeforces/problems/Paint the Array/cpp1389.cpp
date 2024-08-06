b5f18220248b20722a0133aece435f0420084472
Hinski2 <kubailinski2@gmail.com>
2024-08-06 14:47:41 +0200

    2x1100

---
+++ codeforces/problems/Paint the Array/cpp1389.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator+(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first + p2.first, p1.second + p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator-(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first - p2.first, p1.second - p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator*(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first * p2.first, p1.second * p2.second);
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) {
+    os << p.first << ' ' << p.second;
+    return os;
+}
+template <typename T1, typename T2>
+istream& operator>>(istream& is, pair<T1, T2>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+#define int ll
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int solve(){
+    int n; cin >> n;
+    vector<int> v(n); cin >> v;
+    int g1 = v[0], g2 = v[1];
+    for(int i = 0; i < n; i += 2) g1 = __gcd(g1, v[i]);
+    for(int i = 1; i < n; i += 2) g2 = __gcd(g2, v[i]);
+
+    if(g1 != 1){
+        bool ans = true;
+        for(int i = 1; i < n; i += 2)
+            if(v[i] % g1 == 0){
+                ans = false;
+                break;
+            }
+        if(ans) return g1;
+    }
+    if(g2 != 1){
+        bool ans = true;
+        for(int i = 0; i < n; i += 2)
+            if(v[i] % g2 == 0){
+                ans = false;
+                break;
+            }
+        if(ans) return g2;
+    }
+
+    return 0;
+}
+
+signed main(){
+    io; int t; cin >> t;
+    while(t--)
+        cout << solve() << endl;
+}
