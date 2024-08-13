be269e9a5ab1bf34297857c4a70037206908b0bb
Hinski2 <kubailinski2@gmail.com>
2024-08-13 19:18:08 +0200

    contest

---
+++ codeforces/contests/Round 966/c.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator+(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first + p2.first, p1.second + p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator-(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first - p2.first, p1.second - p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator*(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first * p2.first, p1.second * p2.second);
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) {
+    os << p.first << ' ' << p.second;
+    return os;
+}
+template <typename T1, typename T2>
+istream& operator>>(istream& is, pair<T1, T2>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+void solve(){
+    int n; cin >> n;
+    vector<int> v(n); cin >> v;
+    int m; cin >> m;
+
+    while(m--){
+        map<char, int> mapa;
+        map<int, char> mapa2;
+        
+        string s; cin >> s;
+        if(n != s.size()){
+            cout << "NO" << endl;
+            continue;
+        }
+
+        bool ans = true; 
+        for(int i = 0; i < n; i++){
+            if(mapa.count(s[i]) != mapa2.count(v[i])){
+                ans = false;
+                break;
+            }
+            else if(!mapa.count(s[i]) && !mapa2.count(v[i])) mapa[s[i]] = v[i], mapa2[v[i]] = s[i];
+            else if(mapa[s[i]] != v[i] || mapa2[v[i]] != s[i]){
+                ans = false;
+                break;
+            }
+        }
+
+        cout << (ans ? "YES" : "NO") << endl;
+    }
+}
+
+int main(){
+    io; int t; cin >> t;
+    while(t--)
+        solve();
+}
