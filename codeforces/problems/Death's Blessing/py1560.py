b2576aa16fa8d96b9abc5393e4b07ed7857e6bc3
Hinski2 <kubailinski2@gmail.com>
2024-11-10 13:26:50 +0100

    2x800, 1x1200

---
+++ codeforces/problems/Death's Blessing/py1560.py
@@ -0,0 +1,7 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = [int(i) for i in input().split()]
+    b = [int(i) for i in input().split()]
+    print(sum(a) + sum(b) - max(b))
+    
