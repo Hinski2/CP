84007e677de5c85be0ad4a88b5f9ed4ae582cba7
Hinski2 <kubailinski2@gmail.com>
2024-12-27 18:13:55 +0100

    1x800, 1x1100, 1x1300

---
+++ codeforces/problems/Maximal AND/cpp1702.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator+(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first + p2.first, p1.second + p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator-(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first - p2.first, p1.second - p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator*(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first * p2.first, p1.second * p2.second);
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) {
+    os << p.first << ' ' << p.second;
+    return os;
+}
+template <typename T1, typename T2>
+istream& operator>>(istream& is, pair<T1, T2>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+#define int ll
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int n, k;
+int numb[mak];
+
+int poss(int pow){
+    int cnt = 0, x = 1 << pow;
+    for(int i = 0; i < n; i++){
+        if(!(numb[i] & x)) cnt++;
+    }
+
+    if(cnt <= k){
+        k -= cnt;
+        return 1;
+    }
+    return 0;
+}
+
+int solve(){
+    cin >> n >> k;
+    for(int i = 0; i < n; i++) cin >> numb[i];
+
+    for(int i = 30; i >= 0; i--){
+        if(poss(i)){
+            int x = 1 << i;
+            for(int i = 0; i < n; i++){
+                numb[i] |= x;
+            }
+        }
+    }
+
+    int ans = numb[0];
+    for(int i = 1; i < n; i++)
+        ans &= numb[i];
+
+    return ans;
+}
+
+signed main(){
+    io; int t; cin >> t;
+    while(t--)
+        cout << solve() << endl;
+}
---
+++ codeforces/problems/The Clock/cpp1701.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator+(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first + p2.first, p1.second + p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator-(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first - p2.first, p1.second - p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator*(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first * p2.first, p1.second * p2.second);
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) {
+    os << p.first << ' ' << p.second;
+    return os;
+}
+template <typename T1, typename T2>
+istream& operator>>(istream& is, pair<T1, T2>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+bool ok(int tajm){
+    int h = tajm / 60, m = tajm % 60;
+    string sh = to_string(h), sm = to_string(m);
+    if(sh.size() == 1) sh = "0" + sh;
+    if(sm.size() == 1) sm = "0" + sm;
+    reverse(all(sm));
+    return sm == sh;
+}
+
+int solve(){
+    int a, b, n; char c;
+    cin >> a >> c >> b >> n;
+    int tajm = a * 60 + b;
+
+    set<int> s;
+    int ans = 0;
+    while(!s.count(tajm)){
+        if(ok(tajm)) ans++;
+        s.insert(tajm);
+        tajm += n; tajm %= 1440;
+    }
+    return ans;
+}
+
+int main(){
+    io; int t; cin >> t;
+    while(t--)
+        cout << solve() << endl;
+}
