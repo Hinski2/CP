8e473c6f33a7b72043769066e6a4fd54378971b8
Hinski2 <kubailinski2@gmail.com>
2024-12-30 17:12:27 +0100

    1x800, 2x1100

---
+++ codeforces/problems/Sakurako's Hobby/cpp1711.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator+(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first + p2.first, p1.second + p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator-(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first - p2.first, p1.second - p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator*(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first * p2.first, p1.second * p2.second);
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) {
+    os << p.first << ' ' << p.second;
+    return os;
+}
+template <typename T1, typename T2>
+istream& operator>>(istream& is, pair<T1, T2>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int n, p[mak];
+char color[mak];
+vector<int> vis;
+
+int len, black;
+void f(int idx, int stop){
+    len++;
+    black += color[idx] == '0';
+    vis.pb(idx);
+
+    if(p[idx] != stop){
+        f(p[idx], stop);
+    }
+}
+
+void solve(){
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        cin >> p[i];
+    }
+    for(int i = 1; i <= n; i++){
+        cin >> color[i];
+    }
+
+    vector<int> ans(n + 1, -1);
+    for(int i = 1; i <= n; i++){
+        if(ans[i] != -1) continue;
+        len = black = 0;
+        vis.clear();
+        f(i, i);
+        for(auto u: vis){
+            ans[u] = black;
+        }
+    }
+
+    for(int i = 1; i <= n; i++)
+        cout << ans[i] << ' ';
+    cout << endl;
+}
+
+int main(){
+    io; int t; cin >> t;
+    while(t--)
+    solve();
+}
