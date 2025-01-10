3e71f69114624d98e7387936b60097f8e3f01a1c
Hinski2 <kubailinski2@gmail.com>
2025-01-10 16:20:31 +0100

    1x800, 1x1000

--- codeforces/get_testcase.cpp
+++ codeforces/get_testcase.cpp
@@ -1,18 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int testcase_num = 33;
+const int testcase_num = 34;
 
 int main(){
     int t; cin >> t;
     ofstream out("a.out");
     for(int i = 0; i < testcase_num; i++){
         int n; cin >> n;
-        vector<int> v(n); for(auto &u: v) cin >> u;
         if(i + 1 == testcase_num){
-            out  << n << endl;
-            for(auto u: v) out << u << ' ';
-            out << endl;
+            out << n << endl;
         }
     }
     out.close();
