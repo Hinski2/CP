6f34700a0a98542ce088058ef552bff0b8aa2cbf
Hinski2 <kubailinski2@gmail.com>
2025-02-14 18:53:30 +0100

    1x1200, 1x1300

---
+++ codeforces/problems/Sofia and the Lost Operations/cpp1810.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+template<typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator+(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first + p2.first, p1.second + p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator-(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first - p2.first, p1.second - p2.second);
+}
+template <typename T1, typename T2>
+pair<T1, T2> operator*(const pair<T1, T2>& p1, const pair<T1, T2>& p2) {
+    return make_pair(p1.first * p2.first, p1.second * p2.second);
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) {
+    os << p.first << ' ' << p.second;
+    return os;
+}
+template <typename T1, typename T2>
+istream& operator>>(istream& is, pair<T1, T2>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_multiset;
+//erase: oms.erase(oms.upper_bound(value))
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+bool solve(){
+    int n; cin >> n;
+    vector<int> a(n), b(n); cin >> a >> b;
+    int m; cin >> m;
+    vector<int> d(m); cin >> d;
+
+    map<int, int> ma, mb, md;
+    for(auto u: a) ma[u]++;
+    for(auto u: b) mb[u]++;
+    for(auto u: d) md[u]++;
+
+    int can = true;
+    for(auto u: d){
+        if(mb[u] == 0) can = false;
+        else can = true;
+    }
+    if(!can) return 0;
+
+    for(int i = 0; i < n; i++){
+        if(a[i] != b[i]){
+            if(md[b[i]]) md[b[i]]--;
+            else return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main(){
+    io; int t; cin >> t;
+    while(t--)
+        cout << yn << endl;
+}
