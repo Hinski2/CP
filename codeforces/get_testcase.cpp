e35dd964ade25b07a9ad933d234d72d1acba4c45
Hinski2 <kubailinski2@gmail.com>
2025-03-18 13:27:33 +0100

    1x1300

--- codeforces/get_testcase.cpp
+++ codeforces/get_testcase.cpp
@@ -1,19 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int testcase_num = 40;
+const int testcase_num = 38;
 
 int main(){
     int t; cin >> t;
     ofstream out("a.out");
     for(int i = 0; i < testcase_num; i++){
-        int n, k; cin >> n >> k;
-        vector<int> v(n); for(auto &u: v) cin >> u;
+        string n; cin >> n;
         if(i + 1 == testcase_num){
             out << n << endl;
-            for(auto u: v)
-                out << u << ' ';
-            out << endl;
         }
     }
     out.close();
