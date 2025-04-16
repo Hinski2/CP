300b3ecb41eba4c8d2294a6994c1718f66b81dbf
Hinski2 <kubailinski2@gmail.com>
2025-04-16 18:51:02 +0200

    1x800

---
+++ codeforces/problems/Expensive Number/cpp1893.cpp
@@ -0,0 +1,66 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+template<typename T> long long sum(const vector<T> &v){
+    long long s = 0;
+    for(auto u: v) s += u;
+    return s;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? YES : NO)
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int solve(){
+    string s; cin >> s;
+    if(s.size() == 1) return 0;
+
+    int ans;
+    for(int i = s.size() - 1, ile = 0; i >= 0; i--, ile++){
+        if(s[i] == '0') continue;
+        int cnt = 0;
+        for(int j = 0; j < i; j++){
+            if(s[j] != '0') cnt++;
+        }
+
+        ans = ile + cnt;
+        break;
+    }
+
+    return ans;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while(t--)
+        cout << solve() << endl;
+}
+
