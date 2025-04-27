5625c1fddc729ad4796cf4f031301461d436f578
Hinski2 <kubailinski2@gmail.com>
2025-04-27 14:48:00 +0200

    1x1200

---
+++ codeforces/problems/Chat Order/cpp1904.cpp
@@ -0,0 +1,55 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+template<typename T> long long sum(const vector<T> &v){
+    long long s = 0;
+    for(auto u: v) s += u;
+    return s;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? YES : NO)
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n; cin >> n;
+    vector<string> v(n); cin >> v;
+    set<string> m;
+    
+    for(int i = n - 1; i >= 0; i--){
+        if(!m.count(v[i])){
+            cout << v[i] << '\n';
+            m.insert(v[i]);
+        }
+    }
+
+}
+
