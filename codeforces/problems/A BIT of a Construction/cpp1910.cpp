840dd87bdfd58493f6230d3af5c6d30bcd811ef6
Hinski2 <kubailinski2@gmail.com>
2025-05-03 12:12:53 +0200

    1x1100

---
+++ codeforces/problems/A BIT of a Construction/cpp1910.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+template<typename T> long long sum(const vector<T> &v){
+    long long s = 0;
+    for(auto u: v) s += u;
+    return s;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? YES : NO)
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+void solve(){
+    int n, k; cin >> n >> k;
+    if(n == 1){
+        cout << k << endl;
+    } else {
+        int b = 0;
+        for(int i = 30; i >= 0; i--){
+            if(k & (1 << i)){
+                b = i;
+                break;
+            }
+        }
+
+        vector<int>v(n);
+        v[0] = k - (1 << b) + 1;
+        v[1] = (1 << b) - 1;
+        cout << v << endl;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while(t--)
+        solve();
+}
+
