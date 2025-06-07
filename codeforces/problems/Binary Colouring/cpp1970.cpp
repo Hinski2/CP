ef8b1c0be87a6db4f6da460b86ecbe29f7916991
Hinski2 <kubailinski2@gmail.com>
2025-06-07 11:34:51 +0200

    1x1100

---
+++ codeforces/problems/Binary Colouring/cpp1970.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+template<typename T> long long sum(const vector<T> &v){
+    long long s = 0;
+    for(auto u: v) s += u;
+    return s;
+}
+
+template<typename T> T max(const vector<T> &v){
+    T maxi = v[0];
+    for(auto u: v) maxi = max(maxi, u);
+    return maxi;
+}
+
+template<typename T> T min(const vector<T> &v){
+    T mini = v[0];
+    for(auto u: v) mini = min(mini, u);
+    return mini;
+}
+
+template<typename T> unsigned arg_max(const vector<T> &v){
+    T maxi = v[0];
+    unsigned pos = 0;
+    for(unsigned i = 1; i <= v.size(); i++)
+        if(v[i] > maxi){
+            maxi = v[i];
+            pos = i;
+        }
+    return pos;
+}
+
+template<typename T> unsigned arg_min(const vector<T> &v){
+    T mini = v[0];
+    unsigned pos = 0;
+    for(unsigned i = 1; i <= v.size(); i++)
+        if(v[i] < mini){
+            mini = v[i];
+            pos = i;
+        }
+    return pos;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+void solve() {
+    int x; cin >> x;
+    vector<int> v;
+
+    int r = x;
+    while(r) {
+        if(r % 2) v.pb(1);
+        else v.pb(0);
+
+        r /= 2;
+    }
+
+    bool update = true;
+    while(update) {
+        update = false;
+        for(int i = v.size() - 1; i >= 1; i--) {
+            if(i == v.size() - 1 and v[i] == 1 and v[i - 1] == 1) {
+                v.pb(1);
+                v[i] = 0;
+                v[i - 1] = -1;
+                update = 1;
+            }
+
+            if(v[i] == 1 and v[i - 1] == 1) {
+                v[i + 1] = 1;
+                v[i] = 0;
+                v[i - 1] = -1;
+                update = 1;
+            } else if (v[i - 1] == 1 and v[i] == -1) {
+                v[i] = 0;
+                v[i - 1] = -1;
+                update = 1;
+            }
+        }
+    }
+
+    cout << v.size() << endl;
+    cout << v << endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while(t--) 
+        solve();
+}
