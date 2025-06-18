3c1ce02b158f2db0e01d98cfa6fe52adcfcd5e89
Hinski2 <kubailinski2@gmail.com>
2025-06-18 19:44:54 +0200

    1x800

---
+++ codeforces/problems/Above the Clouds/cpp1984.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+template<typename T> long long sum(const vector<T> &v){
+    long long s = 0;
+    for(auto u: v) s += u;
+    return s;
+}
+
+template<typename T> T max(const vector<T> &v){
+    T maxi = v[0];
+    for(auto u: v) maxi = max(maxi, u);
+    return maxi;
+}
+
+template<typename T> T min(const vector<T> &v){
+    T mini = v[0];
+    for(auto u: v) mini = min(mini, u);
+    return mini;
+}
+
+template<typename T> unsigned arg_max(const vector<T> &v){
+    T maxi = v[0];
+    unsigned pos = 0;
+    for(unsigned i = 1; i <= v.size(); i++)
+        if(v[i] > maxi){
+            maxi = v[i];
+            pos = i;
+        }
+    return pos;
+}
+
+template<typename T> unsigned arg_min(const vector<T> &v){
+    T mini = v[0];
+    unsigned pos = 0;
+    for(unsigned i = 1; i <= v.size(); i++)
+        if(v[i] < mini){
+            mini = v[i];
+            pos = i;
+        }
+    return pos;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? "Yes" : "No")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+bool solve() {
+    int n; cin >> n;
+    string s; cin >> s;
+    multiset<int> l, r;
+    for(int i = n - 1; i > 1; i--)
+        r.insert(s[i]);
+
+    l.insert(s[0]);
+
+    for(int i = 1; i + 1 < n; i++) {
+        if(l.contains(s[i]) or r.contains(s[i])) return 1;
+        l.insert(s[i]);
+        r.erase(r.find(s[i + 1]));
+    }
+
+    return 0;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while(t--) {
+        cout << yn << endl;
+    }
+}
