eed47d5caf000bf33e22f3cce323004c51370d63
Hinski2 <kubailinski2@gmail.com>
2025-07-01 12:35:29 +0200

    1x1200

---
+++ codeforces/problems/Virus/cpp1997.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+template<typename T> long long sum(const vector<T> &v){
+    long long s = 0;
+    for(auto u: v) s += u;
+    return s;
+}
+
+template<typename T> T max(const vector<T> &v){
+    T maxi = v[0];
+    for(auto u: v) maxi = max(maxi, u);
+    return maxi;
+}
+
+template<typename T> T min(const vector<T> &v){
+    T mini = v[0];
+    for(auto u: v) mini = min(mini, u);
+    return mini;
+}
+
+template<typename T> unsigned arg_max(const vector<T> &v){
+    T maxi = v[0];
+    unsigned pos = 0;
+    for(unsigned i = 1; i <= v.size(); i++)
+        if(v[i] > maxi){
+            maxi = v[i];
+            pos = i;
+        }
+    return pos;
+}
+
+template<typename T> unsigned arg_min(const vector<T> &v){
+    T mini = v[0];
+    unsigned pos = 0;
+    for(unsigned i = 1; i <= v.size(); i++)
+        if(v[i] < mini){
+            mini = v[i];
+            pos = i;
+        }
+    return pos;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? "YES" : "NO")
+#define int ll
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int solve() {
+    int n, m; cin >> n >> m;
+    vector<int> infected(m); cin >> infected;
+    sort(all(infected));
+
+    vector<int> segments(m);
+    segments[0] = n - infected[m - 1] + infected[0] - 1;
+
+    for(int i = 0; i + 1 < m; i++) {
+        segments[i + 1] = infected[i + 1] - infected[i] - 1;
+    }
+
+    sort(all(segments), greater<int>());
+
+    int t = 0, ans = 0;
+    for(auto segment: segments) {
+        int len = segment - 2*t;
+        if(len <= 0) return n - ans;
+
+        if(len == 1) {
+            t++;
+            ans++;
+        } else {
+            t += 2;
+            ans += len - 1;
+        }
+    }
+
+    return n - ans;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while(t--)
+        cout << solve() << endl;
+}
