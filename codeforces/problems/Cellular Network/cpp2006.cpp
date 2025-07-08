760c559ba55a2ad14ca2c2781a1d59aaf3da7f2e
Hinski2 <kubailinski2@gmail.com>
2025-07-08 15:35:22 +0200

    1x1000, 1x1300, 1x1500

---
+++ codeforces/problems/Cellular Network/cpp2006.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+template<typename T> long long sum(const vector<T> &v){
+    long long s = 0;
+    for(auto u: v) s += u;
+    return s;
+}
+
+template<typename T> T max(const vector<T> &v){
+    T maxi = v[0];
+    for(auto u: v) maxi = max(maxi, u);
+    return maxi;
+}
+
+template<typename T> T min(const vector<T> &v){
+    T mini = v[0];
+    for(auto u: v) mini = min(mini, u);
+    return mini;
+}
+
+template<typename T> unsigned arg_max(const vector<T> &v){
+    T maxi = v[0];
+    unsigned pos = 0;
+    for(unsigned i = 1; i <= v.size(); i++)
+        if(v[i] > maxi){
+            maxi = v[i];
+            pos = i;
+        }
+    return pos;
+}
+
+template<typename T> unsigned arg_min(const vector<T> &v){
+    T mini = v[0];
+    unsigned pos = 0;
+    for(unsigned i = 1; i <= v.size(); i++)
+        if(v[i] < mini){
+            mini = v[i];
+            pos = i;
+        }
+    return pos;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? "YES" : "NO")
+#define int ll
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+vector<int> cities, towers;
+bool check(int mid) {
+    int tower_idx = 0; 
+    for(auto city: cities) {
+        while(tower_idx < towers.size() and abs(towers[tower_idx] - city) > mid)
+            tower_idx++;
+
+        if(tower_idx == towers.size() or abs(towers[tower_idx] - city) > mid)
+            return 0;
+    }
+
+    return 1;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, m; cin >> n >> m;
+    cities.resize(n); cin >> cities;
+    towers.resize(m); cin >> towers;
+
+    int l = 0, r = 2 * inf;
+    while(l < r){
+        int mid = (l + r) / 2;
+        if(check(mid)) r = mid;
+        else l = mid + 1;
+    }
+
+    cout << l << endl;
+}
