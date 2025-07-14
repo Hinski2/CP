a88ddecb939c0392e8fb0f02815a3b36d06fd0ba
Hinski2 <kubailinski2@gmail.com>
2025-07-14 15:54:50 +0200

    2x1400, 1x1500

---
+++ codeforces/problems/Polo the Penguin and Matrix/cpp2026.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+template<typename T> long long sum(const vector<T> &v){
+    long long s = 0;
+    for(auto u: v) s += u;
+    return s;
+}
+
+template<typename T> T max(const vector<T> &v){
+    T maxi = v[0];
+    for(auto u: v) maxi = max(maxi, u);
+    return maxi;
+}
+
+template<typename T> T min(const vector<T> &v){
+    T mini = v[0];
+    for(auto u: v) mini = min(mini, u);
+    return mini;
+}
+
+template<typename T> unsigned arg_max(const vector<T> &v){
+    T maxi = v[0];
+    unsigned pos = 0;
+    for(unsigned i = 1; i <= v.size(); i++)
+        if(v[i] > maxi){
+            maxi = v[i];
+            pos = i;
+        }
+    return pos;
+}
+
+template<typename T> unsigned arg_min(const vector<T> &v){
+    T mini = v[0];
+    unsigned pos = 0;
+    for(unsigned i = 1; i <= v.size(); i++)
+        if(v[i] < mini){
+            mini = v[i];
+            pos = i;
+        }
+    return pos;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 100 + 7;
+
+int mat[mak][mak];
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, m, d, r; cin >> n >> m >> d;
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= m; j++)
+            cin >> mat[i][j];
+    
+    r = mat[1][1] % d;
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= m; j++)
+            if(mat[i][j] % d != r) {
+                cout << -1 << endl;
+                return 0;
+            }
+    
+    vector<int> v;
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= m; j++)
+            v.pb((mat[i][j] - r) / d);
+
+    sort(all(v));
+    int target = v[(v.size() - 1) / 2];
+    int ans = 0;
+    for(auto u: v)
+        ans += abs(u - target);
+    
+    cout << ans << endl;
+}
