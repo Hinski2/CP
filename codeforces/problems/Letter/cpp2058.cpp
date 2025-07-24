0c40c09097565e9f9e63e15bd954c74f6176c015
Hinski2 <kubailinski2@gmail.com>
2025-07-24 17:09:29 +0200

    1x800, 1x1000, 1x1100

---
+++ codeforces/problems/Letter/cpp2058.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v){
+    for(const auto &u: v){
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v){
+    for(auto& u : v){
+        is >> u;
+    }
+    return is;
+}
+
+template<typename T> long long sum(const vector<T> &v){
+    long long s = 0;
+    for(auto u: v) s += u;
+    return s;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? "YES" : "NO")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+bool solve() {
+    string a, b; 
+    getline(cin, a);
+    getline(cin, b);
+
+    map<int, int> aa, bb;
+    for(auto u: a)
+        aa[u]++;
+    for(auto u: b)
+        bb[u]++;
+
+    for(auto [k, v]: bb)
+        if(aa[k] < v and k != ' ') return 0;
+
+    return 1;
+}
+
+int main(){
+    cout << yn << endl;
+}
