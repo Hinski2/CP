5ffcaa9b71de24464b4d35e4e123e1acdd2d1055
Hinski2 <kubailinski2@gmail.com>
2025-07-28 18:12:05 +0200

    1x800, 1x1100, 1x1500

---
+++ codeforces/problems/Limited Repainting/cpp2069.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T> ostream& operator<<(ostream& os, const pair<T, T>& p) {
+    os << p.first << ' ' << p.second << '\n';
+    return os;
+}
+
+template<typename T> istream& operator<<(istream& os, const pair<T, T>& p) {
+    os >> p.first >> p.second;
+    return os;
+}
+
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for(const auto &u: v) {
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for(auto& u : v) {
+        is >> u;
+    }
+    return is;
+}
+
+template<typename T> long long sum_element(const vector<T> &v) {
+    long long s = 0;
+    for(auto u: v) s += u;
+    return s;
+}
+
+template<typename T, typename ... Args> T max(const T& a, const Args& ... args) {
+    return max(a, max(args...));
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? "YES" : "NO")
+#define int ll
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int solve() {
+    int n, k; cin >> n >> k;
+    string s(n + 1, ' '); for(int i = 1; i <= n; i++) cin >> s[i];
+    vector<int> v(n + 1); for(int i = 1; i <= n; i++) cin >> v[i];
+
+    auto check = [&](int mid) { // czyli za mid dostaje penalty
+        int used = 0, usig = 0;
+        for(int i = 1; i <= n; i++) {
+            if(s[i] == 'B') { // B
+                if(v[i] <= mid) continue;
+                else {
+                    if(!usig) used++, usig = 1;
+                }
+            } else { // R
+                if(v[i] > mid) usig = 0;
+            }
+        }
+
+        return used <= k;
+    };
+
+    int l = 0, r = *max_element(all(v));
+    while(l < r) {
+        int mid = (l + r) / 2;
+        if(check(mid)) r = mid;
+        else l = mid + 1;
+    }
+
+    return l;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while(t--)
+        cout << solve() << endl;
+}
