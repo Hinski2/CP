92a17ceb41df5e62aae77dc2b9599a095b29ed92
Hinski2 <kubailinski2@gmail.com>
2025-07-29 14:55:50 +0200

    1x1100, 1x1400, 1x1500

---
+++ codeforces/problems/Final Boss/cpp2073.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T> ostream& operator<<(ostream& os, const pair<T, T>& p) {
+    os << p.first << ' ' << p.second << '\n';
+    return os;
+}
+
+template<typename T> istream& operator<<(istream& os, const pair<T, T>& p) {
+    os >> p.first >> p.second;
+    return os;
+}
+
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for(const auto &u: v) {
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for(auto& u : v) {
+        is >> u;
+    }
+    return is;
+}
+
+template<typename T> long long sum_element(const vector<T> &v) {
+    long long s = 0;
+    for(auto u: v) s += u;
+    return s;
+}
+
+template<typename T, typename ... Args> T max(const T& a, const Args& ... args) {
+    return max(a, max(args...));
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? "YES" : "NO")
+#define int ll
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int solve() {
+    int h, n; cin >> h >> n;
+    vector<pii> att(n); // <att, cd>
+    for(int i = 0; i < n; i++) cin >> att[i].fi;
+    for(int i = 0; i < n; i++) cin >> att[i].se;
+
+    auto check = [&](int mid) -> bool {
+        int damage = 0;
+        for(auto [at, cd]: att) {
+            damage += at * ((mid + cd - 1) / cd);
+            if(damage >= h) return true;
+        }
+
+        return damage >= h;
+    };
+
+    int l = 1, r = h * mak;
+    while(l < r) {
+        int mid = (l + r) / 2;
+        if(check(mid)) r = mid;
+        else l = mid + 1;
+    }
+
+    return l;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while(t--)
+        cout << solve() << endl;
+}
