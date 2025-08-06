b3ab9a2cd1d959592d39d9d14ad880e10dd256bb
Hinski2 <kubailinski2@gmail.com>
2025-08-06 14:39:15 +0200

    2x1200, 1x1500

---
+++ codeforces/problems/Binary String Reconstruction2/cpp2097.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// geometry
+template<typename T>
+ostream& operator<<(ostream& os, const pair<T, T>& p) {
+    os << p.first << ' ' << p.second << '\n';
+    return os;
+}
+
+template<typename T> 
+istream& operator>>(istream& is, pair<T, T>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+template<typename T>
+pair<T, T> operator + (const T& a, const T& b) {
+    return make_pair(a.first + b.first, a.second + b.second);
+}
+
+template<typename T>
+pair<T, T> operator - (const T& a, const T& b) {
+    return make_pair(a.first - b.first, a.second - b.second);
+}
+
+template<typename T>
+T operator * (const T& a, const T& b) {
+    return a.first * b.second - a.second * b.first;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b) {
+    return a * b;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b, const T& c) {
+    return (b - a) * (c - a);
+}
+
+// vector
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for(const auto &u: v) {
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for(auto& u : v) {
+        is >> u;
+    }
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? "YES" : "NO")
+#define int ll
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+void solve() {
+    string w; cin >> w;
+    int x; cin >> x;
+    int n = w.size();
+    string s(n, '#');
+
+    for(int i = 0; i < n; i++) {
+        if(w[i] == '0' and i - x >= 0) s[i - x] = '0';
+        if(w[i] == '0' and i + x < n) s[i + x] = '0';
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(w[i] == '0') continue;
+
+        bool ok = false;
+        if(i - x >= 0 and s[i - x] != '0') s[i - x] = '1', ok = true;
+        if(i + x < n and s[i + x] != '0') s[i + x] = '1', ok = true;
+        if(!ok) {
+            cout << -1 << endl;
+            return;
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+        if(s[i] == '#') s[i] = '0';
+
+    cout << s << endl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while(t--)
+        solve();
+}
---
+++ codeforces/problems/Christmas Spruce/cpp2095.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// geometry
+template<typename T>
+ostream& operator<<(ostream& os, const pair<T, T>& p) {
+    os << p.first << ' ' << p.second << '\n';
+    return os;
+}
+
+template<typename T> 
+istream& operator>>(istream& is, pair<T, T>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+template<typename T>
+pair<T, T> operator + (const T& a, const T& b) {
+    return make_pair(a.first + b.first, a.second + b.second);
+}
+
+template<typename T>
+pair<T, T> operator - (const T& a, const T& b) {
+    return make_pair(a.first - b.first, a.second - b.second);
+}
+
+template<typename T>
+T operator * (const T& a, const T& b) {
+    return a.first * b.second - a.second * b.first;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b) {
+    return a * b;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b, const T& c) {
+    return (b - a) * (c - a);
+}
+
+// vector
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for(const auto &u: v) {
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for(auto& u : v) {
+        is >> u;
+    }
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? "YES" : "NO")
+#define int ll
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 1100;
+
+bool leaf[mak];
+vector<int> G[mak];
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    memset(leaf, 1, mak);
+
+    int n; cin >> n;
+    for(int i = 2; i <= n; i++) {
+        int x; cin >> x;
+        G[x].pb(i);
+        leaf[x] = false;
+    }
+
+    bool ok = true;
+    for(int i = 1; i <= n; i++) {
+        int cnt = 0;
+        for(auto u: G[i])
+            cnt += leaf[u];
+
+        if(!leaf[i] and cnt < 3) ok = false;
+    }
+
+    cout << (ok ? "YES" : "NO") << endl;
+}
