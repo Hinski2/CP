605dc168b523da518299b5d74dbeccae4c4d4526
Hinski2 <kubailinski2@gmail.com>
2025-08-13 15:45:06 +0200

    2x1200, 1x1400

---
+++ codeforces/problems/Hossam and Friends/cpp2115.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// geometry
+template<typename T>
+ostream& operator<<(ostream& os, const pair<T, T>& p) {
+    os << p.first << ' ' << p.second << '\n';
+    return os;
+}
+
+template<typename T> 
+istream& operator>>(istream& is, pair<T, T>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+template<typename T>
+pair<T, T> operator + (const T& a, const T& b) {
+    return make_pair(a.first + b.first, a.second + b.second);
+}
+
+template<typename T>
+pair<T, T> operator - (const T& a, const T& b) {
+    return make_pair(a.first - b.first, a.second - b.second);
+}
+
+template<typename T>
+T operator * (const T& a, const T& b) {
+    return a.first * b.second - a.second * b.first;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b) {
+    return a * b;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b, const T& c) {
+    return (b - a) * (c - a);
+}
+
+// vector
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for(const auto &u: v) {
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for(auto& u : v) {
+        is >> u;
+    }
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? "YES" : "NO")
+#define int ll
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int solve() {
+    int n, m; cin >> n >> m;
+    vector<int> not_friends(n + 1);
+    for(int i = 1; i <= n; i++)
+        not_friends[i] = n;
+
+    for(int i = 0, x, y; i < m; i++) {
+        cin >> x >> y;
+        if(x > y) swap(x, y);
+        not_friends[x] = min(not_friends[x], y - 1);
+    }
+
+    for(int i = n - 1; i; i--) not_friends[i] = min(not_friends[i], not_friends[i + 1]);
+
+    int ans = n;
+    for(int l = 0; l < n; l++) {
+        int r = not_friends[l];
+        ans += r - l;
+    }
+
+    return ans;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while(t--)
+        cout << solve() << endl;
+}
