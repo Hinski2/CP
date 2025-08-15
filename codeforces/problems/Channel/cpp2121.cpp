d6eb78c6bca188b83b5c65b045e5e3bd3ae0b0f6
Hinski2 <kubailinski2@gmail.com>
2025-08-15 10:07:38 +0200

    3x800

---
+++ codeforces/problems/Channel/cpp2121.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// geometry
+template<typename T>
+ostream& operator<<(ostream& os, const pair<T, T>& p) {
+    os << p.first << ' ' << p.second << '\n';
+    return os;
+}
+
+template<typename T> 
+istream& operator>>(istream& is, pair<T, T>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+template<typename T>
+pair<T, T> operator + (const T& a, const T& b) {
+    return make_pair(a.first + b.first, a.second + b.second);
+}
+
+template<typename T>
+pair<T, T> operator - (const T& a, const T& b) {
+    return make_pair(a.first - b.first, a.second - b.second);
+}
+
+template<typename T>
+T operator * (const T& a, const T& b) {
+    return a.first * b.second - a.second * b.first;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b) {
+    return a * b;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b, const T& c) {
+    return (b - a) * (c - a);
+}
+
+// vector
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for(const auto &u: v) {
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for(auto& u : v) {
+        is >> u;
+    }
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? "YES" : "NO")
+#define int ll
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+string solve() {
+    const string yes = "YES", no = "NO", maybe = "MAYBE";
+    int n, a, q; cin >> n >> a >> q;
+    string s; cin >> s;
+
+    if(n == a) return yes;
+    for(int i = 0, cnt = 0; i < q; i++) {
+        if(s[i] == '+') cnt++;
+        else cnt--;
+
+        if(a + cnt >= n) return yes;
+    }
+
+    if(count(all(s), '+') + a >= n) return maybe;
+    return no;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while(t--)
+        cout << solve() << endl;
+}
