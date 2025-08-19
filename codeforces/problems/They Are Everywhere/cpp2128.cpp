8f2ca4f0017cb5c41b9c8e4826b41de8dcfaf0ac
Hinski2 <kubailinski2@gmail.com>
2025-08-19 13:15:02 +0200

    1x1300, 1x1500

---
+++ codeforces/problems/They Are Everywhere/cpp2128.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// geometry
+template<typename T>
+ostream& operator<<(ostream& os, const pair<T, T>& p) {
+    os << p.first << ' ' << p.second << '\n';
+    return os;
+}
+
+template<typename T> 
+istream& operator>>(istream& is, pair<T, T>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+template<typename T>
+pair<T, T> operator + (const T& a, const T& b) {
+    return make_pair(a.first + b.first, a.second + b.second);
+}
+
+template<typename T>
+pair<T, T> operator - (const T& a, const T& b) {
+    return make_pair(a.first - b.first, a.second - b.second);
+}
+
+template<typename T>
+T operator * (const T& a, const T& b) {
+    return a.first * b.second - a.second * b.first;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b) {
+    return a * b;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b, const T& c) {
+    return (b - a) * (c - a);
+}
+
+// vector
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for(const auto &u: v) {
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for(auto& u : v) {
+        is >> u;
+    }
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? "YES" : "NO")
+#define int ll
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+int n, k; 
+string s;
+
+bool check(int len) {
+    map<char, int> mapa;
+    for(int i = 0; i < len; i++)
+        mapa[s[i]]++;
+
+    if(mapa.size() == k) return 1;
+    for(int i = len; i < n; i++) {
+        mapa[s[i - len]]--;
+        mapa[s[i]]++;
+        if(!mapa[s[i - len]]) mapa.erase(s[i - len]);
+        if(mapa.size() == k) return 1;
+    }
+
+    return 0;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n;
+    cin >> s;
+    set<int> bag;
+    for(auto u: s)
+        bag.insert(u);
+
+    k = bag.size();
+
+    int l = k, r = n;
+    while(l < r) {
+        int mid = (l + r) / 2;
+        if(check(mid)) r = mid;
+        else l = mid + 1;
+    }
+
+    cout << l << endl;
+}
