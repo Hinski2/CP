6891c66cb076b7276eb4288b154a2ced1b7808c7
Hinski2 <kubailinski2@gmail.com>
2025-08-25 22:25:31 +0200

    1xletcode because codeforces doesn't response for the whole day

--- codeforces/get_testcase.cpp
+++ codeforces/get_testcase.cpp
@@ -1,15 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int testcase_num = 38;
+// geometry
+template<typename T>
+ostream& operator<<(ostream& os, const pair<T, T>& p) {
+    os << p.first << ' ' << p.second << '\n';
+    return os;
+}
+
+template<typename T> 
+istream& operator>>(istream& is, pair<T, T>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+template<typename T>
+pair<T, T> operator + (const T& a, const T& b) {
+    return make_pair(a.first + b.first, a.second + b.second);
+}
+
+template<typename T>
+pair<T, T> operator - (const T& a, const T& b) {
+    return make_pair(a.first - b.first, a.second - b.second);
+}
+
+template<typename T>
+T operator * (const T& a, const T& b) {
+    return a.first * b.second - a.second * b.first;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b) {
+    return a * b;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b, const T& c) {
+    return (b - a) * (c - a);
+}
+
+// vector
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for(const auto &u: v) {
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for(auto& u : v) {
+        is >> u;
+    }
+    return is;
+}
+
+const int testcase_num = 10;
 
 int main(){
     int t; cin >> t;
     ofstream out("a.out");
     for(int i = 0; i < testcase_num; i++){
-        string n; cin >> n;
+        int n, m; cin >> n >> m;
+        vector<pair<int, int>> v(m);
+
+        for(auto &[x, y]: v)
+            cin >> x >> y;
+
         if(i + 1 == testcase_num){
-            out << n << endl;
+            out << n << ' ' << m << endl;
+            for(auto [x, y]: v)
+                out << x << ' ' << y << endl;
         }
     }
     out.close();
