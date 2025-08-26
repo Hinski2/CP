8fa552fdab76ebb009a3e30f65c35d91525914ab
Hinski2 <kubailinski2@gmail.com>
2025-08-26 16:32:37 +0200

    2x1100

---
+++ codeforces/problems/AND Reconstruction/cpp2142.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// geometry
+template<typename T>
+ostream& operator<<(ostream& os, const pair<T, T>& p) {
+    os << p.first << ' ' << p.second << '\n';
+    return os;
+}
+
+template<typename T> 
+istream& operator>>(istream& is, pair<T, T>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+template<typename T>
+pair<T, T> operator + (const T& a, const T& b) {
+    return make_pair(a.first + b.first, a.second + b.second);
+}
+
+template<typename T>
+pair<T, T> operator - (const T& a, const T& b) {
+    return make_pair(a.first - b.first, a.second - b.second);
+}
+
+template<typename T>
+T operator * (const T& a, const T& b) {
+    return a.first * b.second - a.second * b.first;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b) {
+    return a * b;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b, const T& c) {
+    return (b - a) * (c - a);
+}
+
+// vector
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for(const auto &u: v) {
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for(auto& u : v) {
+        is >> u;
+    }
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? "YES" : "NO")
+#define int ll
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+void solve() {
+    int n; cin >> n;
+    vector<int> b(n - 1); cin >> b;
+    vector<int> a(n);
+
+    a[0] = b[0];
+    a[n - 1] = b[n - 2];
+
+    for(int i = 1; i < n - 1; i++)
+        a[i] = b[i - 1] | b[i];
+
+    bool ok = true; 
+    for(int i = 0; i < n - 1; i++)
+        if(b[i] != (a[i] & a[i + 1])) ok = false;
+    
+    if(!ok) cout << -1 << endl;
+    else cout << a << endl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while(t--)
+        solve();
+}
---
+++ codeforces/problems/XOR Palindromes/cpp2141.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// geometry
+template<typename T>
+ostream& operator<<(ostream& os, const pair<T, T>& p) {
+    os << p.first << ' ' << p.second << '\n';
+    return os;
+}
+
+template<typename T> 
+istream& operator>>(istream& is, pair<T, T>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+template<typename T>
+pair<T, T> operator + (const T& a, const T& b) {
+    return make_pair(a.first + b.first, a.second + b.second);
+}
+
+template<typename T>
+pair<T, T> operator - (const T& a, const T& b) {
+    return make_pair(a.first - b.first, a.second - b.second);
+}
+
+template<typename T>
+T operator * (const T& a, const T& b) {
+    return a.first * b.second - a.second * b.first;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b) {
+    return a * b;
+}
+
+template<typename T>
+T cross_product(const T& a, const T& b, const T& c) {
+    return (b - a) * (c - a);
+}
+
+// vector
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for(const auto &u: v) {
+        os << u << ' ';
+    }
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for(auto& u : v) {
+        is >> u;
+    }
+    return is;
+}
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define endl '\n'
+#define alf 'z' + 1
+#define yn (solve() ? "YES" : "NO")
+#define int ll
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const int mak = 2e5 + 7;
+
+void solve() {
+    int n; cin >> n;
+    string s; cin >> s;
+
+    int cnt = 0;
+    for(int i = 0; i < n / 2; i++)
+        if(s[i] != s[n - i - 1]) cnt++;
+
+
+    vector<int> ans(n + 1, 1);
+    for(int i = 0; i < cnt; i++)
+        ans[i] = ans[n - i] = 0;
+
+    if(n % 2 == 0) {
+        for(int i = cnt + 1; i <= n / 2; i += 2)
+            ans[i] = 0;
+        for(int i = n - cnt - 1; i >= n / 2; i -= 2)
+            ans[i] = 0;
+    }
+
+    for(int i = 0; i <= n; i++)
+        cout << ans[i];
+    cout << endl; 
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while(t--)
+        solve();
+}
